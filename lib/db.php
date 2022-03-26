acfca18281aa381832ff34f13188bb9d3f2ebdb4
Roman <r.cheusov@gmail.com>
2022-03-26 08:43:26 +0000

    Added db class

---
+++ lib/db.php
@@ -0,0 +1,20 @@
+<?php
+class Db {
+    private $host;
+    private $user;
+    private $password;
+
+    function __construct($host, $user, $password) {
+        $this->host = $host;
+        $this->user = $user;
+        $this->password = $password;
+    }
+
+    function createTable() {
+
+    }
+
+    function insertUsers() {
+
+    }
+}
